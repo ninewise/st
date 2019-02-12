9ce4df019b1013e99de7527111e172d308e250ec
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-02-12 14:36:14 +0100

    change font to M+

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Medium-11";
+static char *font = "M+ 1mn medium-11";
 static int borderpx = 2;
 
 /*
